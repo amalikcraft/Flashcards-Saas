4d29fca50f627eb80c4794c98fc08f723973c20a
malik <ahmad.malik@cooper.edu>
2024-08-20 00:23:50 -0400

    first

---
+++ app/page.js
@@ -0,0 +1,145 @@
+import Image from "next/image";
+import getStripe from '@/utils/get-stripe.js'
+import { SignedIn, SignedOut, UserButton} from '@clerk/nextjs'
+import { Container, AppBar, Toolbar, Typography, Button,Box,Grid } from '@mui/material';
+
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <Container maxWidth = "100vw">
+      <Head>
+        <title>Flashcard Saas</title>
+        <meta name = "description" content = "Create flashcard from your test" />
+      </Head>
+
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" style={{flexGrow: 1}}>
+            Flashcard SaaS
+          </Typography>
+          <SignedOut>
+            <Button color="inherit" href="/sign-in">Login</Button>
+            <Button color="inherit" href="/sign-up">Sign Up</Button>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </Toolbar>
+      </AppBar>
+      <Box sx={{textAlign: 'center', my: 4}}>
+        <Typography variant="h2" component="h1" gutterBottom>
+          Welcome to Flashcard SaaS
+        </Typography>
+        <Typography variant="h5" component="h2" gutterBottom>
+          The easiest way to create flashcards from your text.
+        </Typography>
+
+        {/* Get Started */}
+
+        <Button variant="contained" color="primary" sx={{mt: 2, mr: 2}} href="/generate">
+          Get Started
+        </Button>
+
+        {/* Learn More */}
+
+        <Button variant="outlined" color="primary" sx={{mt: 2}}>
+          Learn More
+        </Button>
+      </Box>
+
+      {/* Features */}
+
+      <Box sx={{my: 6, textAlign: 'center'}}>
+        <Typography variant="h4" component="h2" gutterBottom>
+          Features
+        </Typography>
+        <Grid container spacing={4}>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6" gutterBottom>Easy Text Input</Typography>
+            <Typography>
+              Effortlessly create flashcards by simply typing or pasting your text. Our intuitive interface ensures a smooth experience, turning your content into study-ready flashcards in seconds.
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6" gutterBottom>Smart Flashcards</Typography>
+            <Typography>
+              Powered by advanced AI, our flashcards adapt to your learning style, highlighting key concepts and optimizing your study sessions for maximum retention.
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Typography variant="h6" gutterBottom>Accessible Anywhere</Typography>
+            <Typography>
+              Study on the go with flashcards available on all your devices. Whether you’re at home or on the move, your flashcards are always within reach, ensuring you never miss a learning opportunity.
+            </Typography>
+          </Grid>
+        </Grid>
+      </Box>
+
+
+      {/* Pricing */}
+
+
+      <Box sx={{my: 6, textAlign: 'center'}}>
+        <Typography variant="h4"gutterBottom>Pricing</Typography>
+        <Grid container spacing={4}>
+          <Grid item xs={12} md={4}>
+            
+            <Box sx = {{
+              p: 3,
+              border: '1px solid',
+              borderColor: 'grey.300',
+              borderRadius: 2,
+            }}>
+              <Typography variant="h5" gutterBottom>Free</Typography>
+              <Typography variant="h6" gutterBottom>$0 / month</Typography>             
+              <Typography>
+                Access to Basic flascards features and LIMITED storage.
+              </Typography>
+              <Button variant = "contained" color = "primary" sx = {{mt:2}}>
+                Choose Free
+              </Button>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={4}>
+          <Box sx = {{
+              p: 3,
+              border: '1px solid',
+              borderColor: 'grey.300',
+              borderRadius: 2,
+            }}>
+              <Typography variant="h5" gutterBottom>Standard</Typography>
+              <Typography variant="h6" gutterBottom>$5 / month</Typography>             
+              <Typography>
+                Access to AI-Powered Flascards with LIMITED storage.
+              </Typography>
+              <Button variant = "contained" color = "primary" sx = {{mt:2}}>
+                Choose Standard
+              </Button>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={4}>
+          <Box sx = {{
+              p: 3,
+              border: '1px solid',
+              borderColor: 'grey.300',
+              borderRadius: 2,
+            }}>
+              <Typography variant="h5" gutterBottom>Premium</Typography>
+              <Typography variant="h6" gutterBottom>$10 / month</Typography>             
+              <Typography>
+                Access to AI-Powered Flascards and UNLIMITED storage.
+              </Typography>
+              <Button variant = "contained" color = "primary" sx = {{mt:2}}>
+                Choose Premium
+              </Button>
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
+
+    </Container>
+
+
+  );
+}
---
+++ firebase.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDf80AQMdpwD_h2hHTL1EQ_CwPx8UQvfyw",
+  authDomain: "flashcardsaas-69316.firebaseapp.com",
+  projectId: "flashcardsaas-69316",
+  storageBucket: "flashcardsaas-69316.appspot.com",
+  messagingSenderId: "1081044924784",
+  appId: "1:1081044924784:web:2566a626256a0d7c2c37b5",
+  measurementId: "G-Y3BGGM1ZKD"
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const db = getFirestore(app);
+
+export { db };
